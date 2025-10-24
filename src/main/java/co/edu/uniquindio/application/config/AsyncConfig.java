fb0d65d4ea095a71f5fc6b469ae97960fb12a4eb
Kevyn <kevyn.ramirezg@uqvirtual.edu.co>
2025-10-24 14:36:33 -0500

    Undecimo avance

--- src/main/java/co/edu/uniquindio/application/config/AsyncConfig.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.application.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-public class AsyncConfig implements AsyncConfigurer {
-}
